007484b97076c7af5211664b5942f04817f999dd
rupeshgante <105144968+rupeshgante@users.noreply.github.com>
2022-06-02 11:37:33 +0530

    showing users after reload

--- bookingAppoinment/appointment.js
+++ bookingAppoinment/appointment.js
@@ -14,73 +14,66 @@ function additems(e){
    
    axios.post('https://crudcrud.com/api/06c018342675479398d2e9442a5f4f0d/appointmentData',user)
         .then((response)=>{
-              add(response.data);
+              show(response.data);
               console.log(response);
         })
         .catch(err=>console.log(err));
 
-//localStorage.setItem('userdetails'+ email,JSON.stringify(user));
-// show(user);
- }
-// var userlist=document.getElementById('listOfPeople')
 
-//  function show(user){
-//     // e.preventDefault();
+}
 
+document.addEventListener('DOMContentLoaded',()=>{
+
+    axios.get('https://crudcrud.com/api/06c018342675479398d2e9442a5f4f0d/appointmentData')
+    .then((response)=>{
+    for(var i=0;i<response.data.length;i++){
+        show(response.data[i]);
+    }
+    }); 
+    });
+
+ function show(user){
+    // e.preventDefault();
+    var userlist=document.getElementById('listOfPeople')
      
-//     // console.log(d);
-//     var li=document.createElement('li');
-//     li.className='item';
-//     li.id='li'+user.email;
-//     li.appendChild(document.createTextNode(`name:${d.name} email:${d.email}`));
-//     var edit=document.createElement('input');
-//     edit.className='edit';
-//     edit.id='edit';
+    // console.log(d);
+    var li=document.createElement('li');
+    li.className='item';
+    li.id='li'+user.email;
+    li.appendChild(document.createTextNode(`name:${user.name} email:${user.email} Phone :${user.phone}`));
+    var edit=document.createElement('input');
+    edit.className='edit';
+    edit.id='edit';
     
-//     edit.type='button';
-//     edit.value='EDIT';
-//     edit.style.border = "2px solid green";
-//     li.appendChild(edit);
-//    edit.addEventListener('click',()=>{
-//        var x=document.getElementById('edit');
-//      var n=document.getElementById('name');
-//      var e=document.getElementById('email');
-//      n.value=d.name;
-//      e.value=d.email;
-//      localStorage.removeItem('userdetails'+user.email);
-//         li.remove();
-//    })
+    edit.type='button';
+    edit.value='EDIT';
+    edit.style.border = "2px solid green";
+    li.appendChild(edit);
+   edit.addEventListener('click',()=>{
+       var x=document.getElementById('edit');
+     var n=document.getElementById('name');
+     var e=document.getElementById('email');
+     n.value=d.name;
+     e.value=d.email;
+    // localStorage.removeItem('userdetails'+user.email);
+        li.remove();
+   })
 
-//     var del=document.createElement('input');
-//     del.className='delete';
-//     del.id='delete';
+    var del=document.createElement('input');
+    del.className='delete';
+    del.id='delete';
     
-//     del.type='button';
-//     del.value='DELETE';
-//     del.style.border = "2px solid red";
-//     li.appendChild(del);
-//     del.addEventListener('click',()=>{
-//         localStorage.removeItem('userdetails'+user.email);
-//         li.remove();
-//     });
-//     userlist.appendChild(li);
-// }
-// document.addEventListener('DOMContentLoaded',()=>{
-// var keys = Object.keys(localStorage), 
-// i = keys.length; 
-// console.log("keys", keys);
-// let stringifiedDetailsOfPeople, detailsOfPeople;
-// Object.keys(localStorage).forEach((key) => {
-// if (key.match(/userdetails/g)) {
-//   stringifiedDetailsOfPeople = localStorage.getItem(key);
-//   console.log("stringifiedDetailsOfPeople", stringifiedDetailsOfPeople);
-//   detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
-//   console.log("details", detailsOfPeople);
+    del.type='button';
+    del.value='DELETE';
+    del.style.border = "2px solid red";
+    li.appendChild(del);
+    del.addEventListener('click',()=>{
+        //localStorage.removeItem('userdetails'+user.email);
+        li.remove();
+    });
+    userlist.appendChild(li);
+}
 
-//   show(detailsOfPeople);
-// }
-// });
-// });
 // function removeFromScreen(email)
 // {
   
